f1109c5a90fefd8b05909afd5350cab712b1139f
Zakiego <belummikir@gmail.com>
2021-11-10 19:13:43 +0800

    feat: add plugin tailwind @tailwindcss/line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -156,5 +156,9 @@ module.exports = {
   variants: {
     typography: ['dark'],
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
