46d1f86a1070ec8bdd7222775655180c170378bc
Zakiego <belummikir@gmail.com>
2021-11-11 19:24:47 +0800

    feat: edit footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,31 +1,36 @@
-import Link from './Link'
+import { useRouter } from 'next/router'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
+  const router = useRouter().pathname
+
+  const sizeIcon = 6
+
   return (
     <footer>
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+      <div className="flex flex-col items-center mt-16 mb-10">
+        <div className="flex mb-3 space-x-2 text-xs text-gray-500 md:text-sm dark:text-gray-400">
+          Made with {'🤍'} by {' '}
+          <a
+            href={siteMetadata.twitter}
+            className="transition-all duration-300 hover:text-blue-500 dark:hover:text-blue-400 un-effect"
+          >
+            Zakiego
+          </a>
         </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
-        </div>
+        {router == '/about' ? (
+          ''
+        ) : (
+          <div className="flex space-x-6 scale-75 opacity-75 md:scale-100">
+            <SocialIcon kind="github" href={siteMetadata.github} size={sizeIcon} />
+            <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={sizeIcon} />
+            <SocialIcon kind="twitter" href={siteMetadata.twitter} size={sizeIcon} />
+            <SocialIcon kind="medium" href={siteMetadata.medium} size={sizeIcon} />
+            <SocialIcon kind="spotify" href={siteMetadata.spotify} size={sizeIcon} />
+            <SocialIcon kind="youtube" href={siteMetadata.youtube} size={sizeIcon} />
+          </div>
+        )}
       </div>
     </footer>
   )
