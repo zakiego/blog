fc04d0b5f11911e570e46ffbb10601d457c5c2be
Zakiego <belummikir@gmail.com>
2021-11-12 15:29:51 +0800

    feat: remove logo and add router value

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+// import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -8,6 +8,7 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import { ReactNode } from 'react'
 import { useRouter } from 'next/router'
+import ShowBreakpoint from '@/layouts/ShowBreakpoint'
 
 interface Props {
   children: ReactNode
@@ -19,23 +20,15 @@ const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
+        <header className="flex items-center justify-between py-4">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+            <Link href="/" aria-label="Blog Zakiego">
+              <div className="flex items-center justify-between text-2xl font-semibold">
+                {siteMetadata.headerTitle}
               </div>
             </Link>
           </div>
+          <ShowBreakpoint />
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
@@ -43,8 +36,10 @@ const LayoutWrapper = ({ children }: Props) => {
                   key={link.title}
                   href={link.href}
                   className={`${
-                    link.href == router ? 'text-primary-500 dark:text-primary-500' : ''
-                  } font-medium text-gray-900 sm:p-4 dark:text-gray-100 un-effect `}
+                    link.href == router
+                      ? 'text-primary-500 dark:text-primary-500 '
+                      : 'hover:text-primary-500 dark:hover:text-primary-500 un-effect'
+                  } font-medium text-gray-900 sm:p-4 dark:text-gray-100  `}
                 >
                   {link.title}
                 </Link>
