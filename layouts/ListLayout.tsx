8c693c618715ae9c171f6053e18b4b13311011da
Zakiego <belummikir@gmail.com>
2021-11-12 15:28:25 +0800

    feat: re-write blog layout

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -1,9 +1,10 @@
 import Link from '@/components/Link'
-import Tag from '@/components/Tag'
+// import Tag from '@/components/Tag'
 import { ComponentProps, useState } from 'react'
 import Pagination from '@/components/Pagination'
 import formatDate from '@/lib/utils/formatDate'
 import { PostFrontMatter } from 'types/PostFrontMatter'
+import Image from 'next/image'
 interface Props {
   posts: PostFrontMatter[]
   title: string
@@ -21,6 +22,9 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   // If initialDisplayPosts exist, display it if no searchValue is specified
   const displayPosts =
     initialDisplayPosts.length > 0 && !searchValue ? initialDisplayPosts : filteredBlogPosts
+
+  const imgLink =
+    'https://images.unsplash.com/photo-1600775508114-5c30cf911a40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'
 
   return (
     <>
@@ -53,38 +57,51 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
             </svg>
           </div>
         </div>
-        <ul>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary, readingTime, image } = frontMatter
             return (
-              <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-3 xl:col-span-3">
-                    <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-gray-900 dark:text-gray-100 un-effect"
-                        >
-                          {title}
-                        </Link>
-                      </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
+              <li key={slug} className="py-6 md:py-8 ">
+                <article className="flex items-center justify-between">
+                  <div className="w-7/12 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                    <div className="space-y-4 md:space-y-5 xl:col-span-3">
+                      <div className="space-y-5 md:space-y-6">
+                        <div>
+                          <h2 className="font-bold md:text-2xl">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="py-2 text-gray-900 dark:text-gray-100 un-effect"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                        </div>
+                        <div className="text-sm prose text-gray-500 md:text-base max-w-none dark:text-gray-400 line-clamp-2 md:line-clamp-3">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium ">
+                        <dl>
+                          <dd className="text-xs font-medium leading-6 text-gray-500 md:text-sm dark:text-gray-400">
+                            <dt className="sr-only">Published on</dt>
+                            <time dateTime={date}>{formatDate(date)}</time>
+                            {' · '}
+                            <dt className="sr-only">Reading time</dt>
+                            <span>Dibaca {readingTime} menit</span>
+                          </dd>
+                        </dl>
                       </div>
                     </div>
-                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                      {summary}
-                    </div>
+                  </div>
+                  <div className="block w-1/12" />
+                  <div className="flex-none w-4/12 aspect-w-5 lg:aspect-w-6 aspect-h-1">
+                    <Image
+                      src={image || imgLink}
+                      layout="fill"
+                      alt="Image"
+                      className="object-cover rounded-md"
+                    />
                   </div>
                 </article>
               </li>
