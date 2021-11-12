48f6b13d28cc1edb4780409aae45f110dada544f
Zakiego <belummikir@gmail.com>
2021-11-12 15:31:06 +0800

    feat: add selection colour style

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,9 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return (
+    <div className="max-w-3xl px-4 mx-auto selection:text-gray-100 selection:bg-primary-700 dark:selection:text-gray-200 dark:selection:bg-green-600 sm:px-6 xl:max-w-5xl xl:px-0">
+      {children}
+    </div>
+  )
 }
