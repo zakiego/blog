b479063a2adea66724ae94340c8e58da041ff901
Zakiego <belummikir@gmail.com>
2021-11-12 15:27:59 +0800

    feat: re-write post layout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -32,7 +32,7 @@ interface Props {
 }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }: Props) {
-  const { slug, fileName, date, title, tags } = frontMatter
+  const { slug, fileName, date, title, tags, image, readingTime } = frontMatter
 
   return (
     <SectionContainer>
@@ -44,20 +44,35 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
+            <div className="space-y-2 md:text-center">
+              <div className="pb-5 lg:pt-0">
+                {image && (
+                  <div className="shadow-md aspect-w-2 aspect-h-1 md:aspect-w-3 md:aspect-h-1 2xl:aspect-w-16 2xl:aspect-h-5">
+                    <Image
+                      src={image}
+                      alt="Image"
+                      layout="fill"
+                      className="object-cover rounded-md "
+                    />
+                  </div>
+                )}
+              </div>
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
+              <dl className="space-y-10">
+                <div>
+                  <dd className="text-sm font-medium leading-6 text-gray-500 md:text-base dark:text-gray-400">
+                    <dt className="sr-only">Published on</dt>
+                    <time dateTime={date}>
+                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                    </time>
+                    {' · '}
+                    <dt className="sr-only">Reading time</dt>
+                    <span>Dibaca {readingTime} menit</span>
+                  </dd>
+                </div>
+              </dl>
             </div>
           </header>
           <div
@@ -67,7 +82,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
               <dd>
-                <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
+                <ul className="flex space-x-8 md:justify-center xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
                   {authorDetails.map((author) => (
                     <li className="flex items-center space-x-2" key={author.name}>
                       {author.avatar && (
