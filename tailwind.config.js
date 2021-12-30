a392c5ec578f5e71d374f87ba2ec278aaeb0fe54
Zakiego <belummikir@gmail.com>
2021-12-30 19:35:41 +0800

    fix: error dark mode because delete darkMode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ const colors = require('tailwindcss/colors')
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx', './lib/**/*.ts'],
+  darkMode: 'class',
   theme: {
     extend: {
       spacing: {
