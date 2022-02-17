6002a2de1152357688fa9d26f65467d157ce9d64
Zakiego <belummikir@gmail.com>
2022-02-17 19:41:41 +0800

    feat: change padding-x mobile

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <div className="max-w-3xl px-4 mx-auto selection:text-gray-100 selection:bg-primary-700 dark:selection:text-gray-200 dark:selection:bg-green-600 sm:px-6 xl:max-w-5xl xl:px-0">
+    <div className="max-w-3xl px-3 mx-auto  selection:text-gray-100 selection:bg-primary-700 dark:selection:text-gray-200 dark:selection:bg-green-600 sm:px-6 xl:max-w-5xl xl:px-0">
       {children}
     </div>
   )
