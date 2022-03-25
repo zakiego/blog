be9d987b3aa50cd920801b84599960ed1e07a3fe
Zakiego <belummikir@gmail.com>
2022-03-26 07:28:16 +0800

    feat: change rss

--- lib/generate-rss.ts
+++ lib/generate-rss.ts
@@ -5,13 +5,10 @@ import { PostFrontMatter } from 'types/PostFrontMatter'
 
 const generateRssItem = (post: PostFrontMatter) => `
   <item>
-    <guid>${siteMetadata.siteUrl}/post/${post.slug}</guid>
     <title>${escape(post.title)}</title>
     <link>${siteMetadata.siteUrl}/post/${post.slug}</link>
     ${post.summary && `<description>${escape(post.summary)}</description>`}
     <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-    <author>${siteMetadata.email} (${siteMetadata.author})</author>
-    ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
   </item>
 `
 
@@ -22,8 +19,6 @@ const generateRss = (posts: PostFrontMatter[], page = 'feed.xml') => `
       <link>${siteMetadata.siteUrl}/post</link>
       <description>${escape(siteMetadata.description)}</description>
       <language>${siteMetadata.language}</language>
-      <managingEditor>${siteMetadata.email} (${siteMetadata.author})</managingEditor>
-      <webMaster>${siteMetadata.email} (${siteMetadata.author})</webMaster>
       <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
       <atom:link href="${siteMetadata.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
       ${posts.map(generateRssItem).join('')}
