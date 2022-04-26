91ed238ffd726c0e5e51df54ed773319e246d2bf
Zakiego <belummikir@gmail.com>
2022-04-27 07:57:44 +0800

    feat: change seo title page, add author

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -19,18 +19,19 @@ interface CommonSEOProps {
 
 const CommonSEO = ({ title, description, ogType, ogImage, twImage }: CommonSEOProps) => {
   const router = useRouter()
+
+  const meta = { title: `${title} ${router.pathname != '/' ? `- ${siteMetadata.author}` : ''}` }
+
   return (
     <Head>
-      <title>
-        {title} - {siteMetadata.author}
-      </title>
+      <title>{meta.title}</title>
       <meta name="robots" content="follow, index" />
       <meta name="description" content={description} />
       <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
       <meta property="og:type" content={ogType} />
       <meta property="og:site_name" content={siteMetadata.title} />
       <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={meta.title} />
       {Array.isArray(ogImage) ? (
         ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
       ) : (
@@ -38,7 +39,7 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage }: CommonSEOPr
       )}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={siteMetadata.twitter} />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={twImage} />
     </Head>
